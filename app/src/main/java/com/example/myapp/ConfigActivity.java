0f33e06447e525eafee49f56fd24f253e8879864
roan <2740593710@qq.com>
2024-04-10 22:07:32 +0800

    7_rate

---
+++ app/src/main/java/com/example/myapp/ConfigActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfigActivity extends AppCompatActivity {
+    float dollar1,euro1,won1;
+    private static final String TAG = "Rate";
+    EditText dollar_edit,euro_edit,won_edit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+        //接收参数
+        Intent intent = getIntent();
+        float dollar2 = intent.getFloatExtra("dollar_rate_key",1.1f);
+        float euro2 = intent.getFloatExtra("euro_rate_key",1.1f);
+        float won2 = intent.getFloatExtra("won_rate_key",1.1f);
+        //生成日志
+        Log.i(TAG,"onCreate:dollar2"+dollar2);
+        Log.i(TAG,"onCreate:euro2"+euro2);
+        Log.i(TAG,"onCreate:won2"+won2);
+        //显示在控件中
+        dollar_edit = findViewById(R.id.dollar_edit);
+        euro_edit = findViewById(R.id.euro_edit);
+        won_edit = findViewById(R.id.won_edit);
+
+        dollar_edit.setText(String.valueOf(dollar2));
+        euro_edit.setText(String.valueOf(euro2));
+        won_edit.setText(String.valueOf(won2));
+    }
+    public void configClick(View btn){
+        Button btn_save = findViewById(R.id.btn_save);
+
+        //获取用户输入
+        dollar_edit = findViewById(R.id.dollar_edit);
+        euro_edit = findViewById(R.id.euro_edit);
+        won_edit = findViewById(R.id.won_edit);
+
+        //转换为float
+        dollar1  = Float.parseFloat(dollar_edit.getText().toString());
+        euro1 = Float.parseFloat(euro_edit.getText().toString());
+        won1 = Float.parseFloat(won_edit.getText().toString());
+
+        //传送参数
+        Intent intent1 = new Intent(ConfigActivity.this,MoneyActivity.class);
+        intent1.putExtra("dollar_rate",dollar1);
+        intent1.putExtra("euro_rate",euro1);
+        intent1.putExtra("won_rate",won1);
+        startActivity(intent1);
+    }
+}
---
+++ app/src/main/java/com/example/myapp/MoneyActivity.java
@@ -0,0 +1,79 @@
+package com.example.myapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoneyActivity extends AppCompatActivity {
+    private static final String TAG = "Rate";
+    public float dollarRate = 0.1383f;
+    public float euroRate = 0.1271f;
+    public float wonRate = 186.91f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_money);
+        //法1：intent方法
+        //接收参数
+        Intent config1 = getIntent();
+        dollarRate = config1.getFloatExtra("dollar_rate",0.1383f);
+        euroRate = config1.getFloatExtra("euro_rate",0.1271f);
+        wonRate = config1.getFloatExtra("won_rate",186.91f);
+    }
+
+    public void MoneyClick(View btn){
+        //获取按钮
+        Button btn_dollar = findViewById(R.id.btn_dollar);
+        Button btn_euro = findViewById(R.id.btn_euro);
+        Button btn_won = findViewById(R.id.btn_won);
+        //获取收入数据
+        EditText rmb_input = findViewById(R.id.rmb);
+        TextView result = findViewById(R.id.result);
+        String rmb = rmb_input.getText().toString();
+        //判断用户输入数据是否正确
+        if(rmb.isEmpty()){
+            //提示消息(显示时长由LENGTH_LONG决定)
+            Toast.makeText(this,"请您输入正确的数据!", Toast.LENGTH_LONG).show();
+            //不加show不能在日志的用户界面里出现
+        }
+        else{
+            float input = Float.parseFloat(rmb);
+            float rmb_result = 0f;
+            //判断用户按钮
+            if(btn.getId() == R.id.btn_dollar){
+                rmb_result = input * dollarRate;
+            }
+            else if (btn.getId() == R.id.btn_euro) {
+                rmb_result = input * euroRate;
+            }
+            else if (btn.getId() == R.id.btn_won) {
+                rmb_result = input * wonRate;
+            }
+            //结果输出
+            String rmb_str_result = String.format("%.2f",rmb_result);
+            result.setText(rmb_str_result);
+        }
+    }
+    public void clickOpen(View v){
+
+        //打开新窗口并传入参数
+        Intent config = new Intent(MoneyActivity.this, com.example.myapp.ConfigActivity.class);
+        //传递参数
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
+        startActivity(config);
+        //生成日志
+        Log.i(TAG,"clickOpen:dollarRate=" + dollarRate);
+        Log.i(TAG,"clickOpen:euroRate=" + euroRate);
+        Log.i(TAG,"clickOpen:wonRate=" + wonRate);
+    }
+}
